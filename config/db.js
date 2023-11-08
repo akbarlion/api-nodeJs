e506537f9e3fe6501bc66542b076fb6599548064
Akbar Lion <muhammadakbarlion@gmail.com>
2023-11-08 14:12:59 +0700

    commit

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const conn = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "fake",
+    charset: "utf8mb4",
+    timezone: "+07:00"
+});
+
+conn.getConnection((error) => {
+    if (error) throw error
+    console.log('DB Connected');
+})
